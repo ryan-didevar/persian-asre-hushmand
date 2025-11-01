4b450c8f87802c070066b48616d17c579b103e47
Ryan Didevar <46435931+ryan-didevar@users.noreply.github.com>
2025-11-02 00:16:53 +0330

    Navbar has been implemented

---
+++ app/campaigns/page.tsx
@@ -0,0 +1,5 @@
+const Campings = () => {
+  return <div>Camping</div>;
+};
+
+export default Campings;
---
+++ app/components/NavLogo.tsx
@@ -0,0 +1,9 @@
+const NavLogo = () => {
+  return (
+    <button className="bg-[#ECECEC] w-[102px] h-10 text-gray-800 font-bold rounded-2xl text-lg hover:bg-[#dfdfdf] transition duration-150 ease-in-out">
+      logo
+    </button>
+  );
+};
+
+export default NavLogo;
